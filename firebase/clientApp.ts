d3a67412151047bfd4919cd6cda2e8c122d12938
abdullaherek <abdullah.erek@ogrenci.bartin.edu.tr>
2022-04-13 21:48:31 +0300

    firebase import

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const clientCredentials = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,7 +11,7 @@ const clientCredentials = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-if (!firebase.getApps.length) {
+if (!firebase.apps.length) {
   firebase.initializeApp(clientCredentials);
 }
 
